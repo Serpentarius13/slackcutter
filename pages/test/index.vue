366d47011464fab1cad4e75b3ced1bfb1c679111
Serpentarius13 <serpentarium13@mail.ru>
2023-06-20 12:43:14 +0300

    !

--- pages/test/index.vue
+++ pages/test/index.vue
@@ -7,9 +7,9 @@
 <script setup lang="ts">
 const router = useRouter();
 
-onMounted(() => {
-  if (process.env.NODE_ENV !== "development") router.push("/");
-});
+// onMounted(() => {
+//   if (process.env.NODE_ENV !== "development") router.push("/");
+// });
 </script>
 
 <style scoped lang="scss"></style>
