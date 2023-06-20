3c15a57a97ee5f24a110ffcef2a04d882ea6cab2
Serpentarius13 <serpentarium13@mail.ru>
2023-06-20 12:41:42 +0300

    !

--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <SharedUiVideo video-url="https://www.youtube.com/watch?v=AI9gHN-qa6w" />
+  <ClientOnly>
+    <SharedUiVideo video-url="https://www.youtube.com/watch?v=AI9gHN-qa6w" />
+  </ClientOnly>
 </template>
 
 <script setup lang="ts">
